96c030392032c491756090841add27c6f8cbc317
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-05-11 14:36:50 +0700

    Изменение

--- zad5.cpp
+++ zad5.cpp
@@ -1,33 +1,55 @@
 #include <iostream>
+#include <Windows.h>
+#include <random>
+#include <vector>
 using namespace std;
-
-class BBSGenerator {
-private:
-    unsigned long p, q, M, seed;
-
-public:
-    BBSGenerator(unsigned long pValue, unsigned long qValue, unsigned long initialSeed) : p(pValue), q(qValue), seed(initialSeed) {
-        M = p * q;
+//Инициализация таблиц
+void initialize(int* S, int* K, int s) {
+    for (int i = 0; i < s; i++) {
+        S[i] = i;
     }
-
-    unsigned long generateRandomNumber() {
-        seed = (seed * seed) % M;
-        return seed;
+    int j = 0;
+    for (int i = 0; i < s; i++) {
+        j = (j + S[i] + K[i]) % s;
+        swap(S[i], S[j]);
     }
-};
+}
+// Алгоритм псевдослучайной генерации
+void generationAlgorithm(int* S, vector<int>& output, int n) {
+    int i = 0;
+    int j = 0;
+    for (int k = 0; k < n; k++) {
+        i = (i + 1) % n;
+        j = (j + S[i]) % n;
+        swap(S[i], S[j]);
+        int t = (S[i] + S[j]) % n;
+        int K = S[t];
+        output.push_back(K);
+    }
+}
 
 int main() {
-    unsigned long pValue = 19, qValue = 23; // Простые числа p и q
-    unsigned long initialValue = 7; // Начальное значение для генератора
+    system ("chcp 65001");
+    //2^i
+	const int n=32;
+	int S[n];
+	// Значения ключа K
+	int K[n];
+    // Заполнение ключа K
+    for (int i = 0; i < n;i++) {
+        K[i] = i;
+    }
+    //Инициализация таблиц
+    initialize(S, K, n);
 
-    BBSGenerator bbs(pValue, qValue, initialValue);
+    // Алгоритм псевдослучайной генерации
+    vector<int> output;
+    generationAlgorithm(S, output, n);
 
-    // Генерация псевдослучайных чисел
-    cout << bbs.generateRandomNumber() << " ";
-    cout << bbs.generateRandomNumber() << " ";
-    cout << bbs.generateRandomNumber() << " ";
-    cout << bbs.generateRandomNumber() << " ";
-    cout << bbs.generateRandomNumber() << endl;
-
+    // Вывод сгенерированных чисел
+    cout << "Сгенерированные числа: "<<endl;
+    for (int i = 0; i<output.size(); i++) {
+        cout << i+1 << ": " << output[i] << endl;
+    }
     return 0;
 }
